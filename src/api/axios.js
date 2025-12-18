46247268fd395bf3081b3842bada228b6ede0182
Manjunath072004 <manjunatha.l@y4elabs.com>
2025-12-18 15:49:10 +0530

    resolve invalid token error during login and signup

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,19 +1,30 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://127.0.0.1:8000/api",
-    headers: {
-        "Content-Type":"application/json",
-    },
+  baseURL: "http://127.0.0.1:8000/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
-/*  AUTO-ATTACH JWT TOKEN */
 API.interceptors.request.use(
   (config) => {
+    //  Do NOT attach token for auth-related APIs
+    if (
+      config.url.includes("/auth/login") ||
+      config.url.includes("/auth/signup") ||
+      config.url.includes("/auth/verify") ||
+      config.url.includes("/auth/resend") ||
+      config.url.includes("/auth/check")
+    ) {
+      return config;
+    }
+
     const token = localStorage.getItem("access");
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
+
     return config;
   },
   (error) => Promise.reject(error)
